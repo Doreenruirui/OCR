458cf48cd166b65b1c531557d938e7b2498b4aab
Doreenruirui <ruiruidong1989@gmail.com>
2017-06-28 20:09:21 -0400

    second commit

---
+++ my_utils.py
@@ -0,0 +1,49 @@
+import numpy as np
+from os.path import join
+
+
+def error_rate(P, nthread, flag_char, list_x, list_y):
+    dis_xy = align_pair(P, list_x, list_y, nthread, flag_char=flag_char)
+    res = []
+    for i in range(len(dis_xy)):
+        res.append(dis_xy[i], len(x[i]), len(y[i]))
+    return res
+
+
+def split_with_ratio(num_data, ratio):
+    num_train = int(np.floor(num_data * ratio))
+    rand_index = np.arange(num_data)
+    np.random.shuffle(rand_index)
+    index_train = rand_index[:num_train]
+    index_test = rand_index[num_train:]
+    return index_train, index_test
+
+
+def read_lines(fn, list_str, begin, end):
+    line_id = 0
+    if end != -1:
+        for line in file(fn):
+            if line_id >= begin and line_id < end:
+                list_str.append(line[:-1])
+            line_id += 1
+    else:
+        for line in file(fn):
+            if line_id >= begin:
+                list_str.append(line[:-1])
+            line_id += 1
+
+
+def error_rate_line(file_error, file_ocr, file_truth, begin, end, nthread=40, flag_char=1):
+    list_ocr = []
+    list_truth = []
+    read_lines(file_ocr, list_ocr, begin, end)
+    read_lines(file_truth, list_truth, begin, end)
+    with open(file_error, 'w') as f_:
+        P = Pool(nthread)
+        res = error_rate(P, nthread, flag_char, list_ocr, list_truth)
+        for dis, lx, ly in res:
+            f_.write('%d\t%d\t%d\n' % (dis, lx, ly))
+
+
+
+
--- process_out_of_domain.py
+++ process_out_of_domain.py
@@ -1,122 +1,137 @@
-from os.path import join
-import json
+from PyLib import operate_file as opf
 from levenshtein import align_pair
-from multiprocessing import Pool
+from my_utils import error_rate_line
+from collections import OrderedDict
+from os.path import join, exists
+import  numpy as np
+import json
 import os
 import re
-from collections import OrderedDict
-from PyLib import operate_file as opf
-import  numpy as np
+import sys
 
-
-def error_rate(P, nthread, flag_char, list_x, list_y, len_y):
-    dis_xy = align_pair(P, list_x, list_y, nthread, flag_char=flag_char)
-    res = []
-    for i in range(len(dis_xy)):
-        res.append(dis_xy[i] * 1. / len_y[i])
-    return res
+folder_data = '/scratch/dong.r/Dataset/OCR/eng'
+dict_cat2name = {'m': 'monograph', 'p': 'periodical', 'o': 'other'}
 
 
 def load_pairs_folder():
     keys = ['b1', 'e1', 'b2', 'e2']
-    folder_data = '/scratch/dong.r/Dataset/OCR/eng'
-    info_file = open(join(folder_data, 'pair_info'), 'w')
-    pair_file = open(join(folder_data, 'pairs'), 'w')
-    num_pair = 0
-    list_str1 = []
-    list_str2 = []
+    pair_file = {}
+    folder_name = {}
+    for cat in dict_cat2name:
+        folder_name[cat] = join(folder_data, dict_cat2name[cat])
+        if not exists(folder_name[cat]):
+            os.makedirs(folder_name[cat])
+        pair_file[cat] = {}
+        for key in ['x', 'y', 'info']:
+            pair_file[cat][key] = open(join(folder_name[cat], 'pair_' + key), 'w')
     files = [f for f in os.listdir(join(folder_data, 'pass.json'))]
-    files = [ele for ele in files if os.path.isfile(join(folder_data, 'pass.json', ele))]
     pair_id = 0
     line_id = 0
+    num_other = 0
     for fname in files:
         if fname.startswith('part-'):
             for line in file(join(folder_data, 'pass.json', fname)):
                 cur_dict = json.loads(line.strip())
-                num_pair += len(cur_dict['pairs'])
                 for pair in cur_dict['pairs']:
-                    str1 = re.sub('\n', '<NEWLINE>', pair['_1'])
-                    str2 = re.sub('\n', '<NEWLINE>', pair['_2'])
-                    pair_file.write(str1.encode('utf-8') + '\t\t' + str2.encode('utf-8') + '\n')
-                    cur_info = cur_dict['id1'] + '\t' + cur_dict['id2'] + '\t' + '\t'.join(map(str, [cur_dict[ele] for ele in keys]))
-                    info_file.write(cur_info + '\n')
-                    list_str1.append(str1.encode('utf-8'))
-                    list_str2.append(str2.encode('utf-8'))
+                    str1 = re.sub('\n', ' ', pair['_1'].strip())
+                    str2 = re.sub('\n', ' ', pair['_2'].strip())
+                    if 'periodical' not in cur_dict['id2'] and 'monograph' not in cur_dict['id2']:
+                        cur_key = 'o'
+                        num_other += 1
+                        cur_info = cur_dict['id1'] + '\t' \
+                               +  cur_dict['id2'] + '\t0\t' \
+                               + '\t'.join(map(str, [cur_dict[ele] for ele in keys]))
+                    else:
+                        list_info = cur_dict['id2'].split('_')
+                        if list_info[1] == 'periodical':
+                            cur_key = 'p'
+                        elif list_info[1] == 'monograph':
+                            cur_key = 'm'
+                        if len(list_info) == 3:
+                            cur_task = 0
+                        else:
+                            if list_info[-2] == 'task1':
+                                cur_task = 1
+                            elif list_info[-2] == 'task2':
+                                cur_task = 2
+                        cur_info = cur_dict['id1'] + '\t' \
+                               +  list_info[-1] + '\t'\
+                               + str(cur_task) + '\t' \
+                               + '\t'.join(map(str, [cur_dict[ele] for ele in keys]))
+                    pair_file[cur_key]['x'].write(str1.encode('utf-8') + '\n')
+                    pair_file[cur_key]['y'].write(str2.encode('utf-8') + '\n')
+                    pair_file[cur_key]['info'].write(cur_info + '\n')
                     pair_id += 1
                 line_id += 1
-    print line_id
-    print pair_id
-    info_file.close()
-    pair_file.close()
-    pool = Pool(50)
-    len_y = [len(ele) for ele in list_str1]
-    dis_xy = error_rate(pool, 50, 1, list_str1, list_str2, len_y)
-    dis_file = open(join(folder_data, 'pairs_error'), 'w')
-    for ele in dis_xy:
-        dis_file.write('\t%.5f\n' % ele)
-    dis_file.close()
-    print (num_pair)
+    print num_other
+    np.savetxt(join(folder_data, 'statistics'), np.asarray([line_id, pair_id]))
+    for cat in pair_file:
+        for fh in pair_file[cat]:
+            fh.close()
 
 
-def get_train_data():
-    folder_data = '/scratch/dong.r/Dataset/OCR/eng'
+def compute_error_rate(cat_name, begin, end):
+    folder_name = join(folder_data, dict_cat2namep[cat_name])
+    file_ocr = join(folder_name, 'pairs_x')
+    file_truth = join(folder_name, 'pairs_y')
+    folder_error = join(folder_name, 'error')
+    if not exists(folder_error):
+        os.makedirs(folder_error)
+    file_error = join(folder_error, 'error_%d_%d' % (begin, end))
+    error_rate_line(file_error, file_ocr, file_truth, begin, end)
+
+
+def get_train_data(cat_name):
+    folder_cat = dict_cat2name[cat_name]
     dict_page_pair = OrderedDict()
     line_id = 0
-    line2task = []
-    for line in file(join(folder_data, 'pair_info')):
+    for line in file(join(folder_data, folder_cat, 'pair_info')):
         list_items = line.strip().split('\t')
-        task = list_items[1]
-        b1 = int(list_items[2])
-        b2 = int(list_items[3])
-        key = (b1, b2)
-        if '_task1' in task:
-            line2task.append(1)
-        elif '_task2' in task:
-            line2task.append(2)
-        else:
-            line2task.append(0)
+        key = list_items[1]
         if key not in dict_page_pair:
             dict_page_pair[key] = []
         dict_page_pair[key].append(line_id)
         line_id += 1
-    with open(join(folder_data, 'pairs_date'), 'w') as f_:
+    with open(join(folder_data, folder_cat, 'pair_index'), 'w') as f_:
         for key in dict_page_pair:
-            f_.write(str(key[0]) + '\t' + str(key[1]))
+            f_.write(str(key[0]))
             for value in dict_page_pair[key]:
                 f_.write('\t' + str(value))
             f_.write('\n')
-    np.save(join(folder_data, 'line2task'), np.asarray(line2task))
 
 
-def write_file():
-    folder_data = '/scratch/dong.r/Dataset/OCR/eng'
-    fx_train = open(join(folder_data, 'train.x.txt'), 'w')
-    fy_train = open(join(folder_data, 'train.y.txt'), 'w')
-    fx_test1 = open(join(folder_data, 'test.x.txt.1'), 'w')
-    fy_test1 = open(join(folder_data, 'test.y.txt.1'), 'w')
-    fx_test2 = open(join(folder_data, 'test.x.txt.2'), 'w')
-    fy_test2 = open(join(folder_data, 'test.y.txt.2'), 'w')
-    line2task = np.load(join(folder_data, 'line2task.npy'))
+def write_file(cat_name, postfix):
+    folder_cat = dict_cat2name[cat_name]
+    f_train = open(join(folder_data, folder_cat, 'train.'+ postfix + '.txt'), 'w')
+    if cat_name == 'm' or cat_name == 'p':
+        f_test1 = open(join(folder_data, folder_cat, 'test.'+ postfix + '.txt.1'), 'w')
+        f_test2 = open(join(folder_data, folder_cat, 'test.'+ postfix + '.txt.2'), 'w')
+    line2task = []
+    for line in file(join(folder_data, folder_cat, 'pair_info')):
+        list_items = line.strip().split('\t')
+        line2task.append(int(list_items[2]))
     line_id = 0
-    for pair in file(join(folder_data, 'pairs')):
-        y, x = pair[-1].split('\t')
+    for line in file(join(folder_data, 'pair_' + postfix)):
         if line2task[line_id] == 0:
-            fx_train.write(x + '\n')
-            fy_train.write(y + '\n')
+            f_train.write(x + '\n')
         elif line2task[line_id] == 1:
-            fx_test1.write(x + '\n')
-            fy_test1.write(y + '\n')
+            f_test1.write(x + '\n')
         else:
-            fx_test2.write(x + '\n')
-            fy_test2.write(y + '\n')
-    fx_train.close()
-    fy_train.close()
-    fx_test1.close()
-    fy_test1.close()
-    fx_test2.close()
-    fy_test2.close()
+            f_test2.write(x + '\n')
+        line_id += 1
+    f_train.close()
+    f_test1.close()
+    f_test2.close()
 
 
-# load_pairs_folder()
-get_train_data()
-write_file()
+if sys.argv[1] == 'load':
+    load_pairs_folder()
+elif sys.argv[1] == 'error':
+    cat_name = sys.argv[2]
+    begin = int(sys.argv[3])
+    end = int(sys.argv[4])
+    compute_error_rate(cat_name, begin, end)
+elif sys.argv[1] == 'split':
+    cat_name = sys.argv[2]
+    post_fix = sys.argv[3]
+    write_file(cat_name, post_fix)
