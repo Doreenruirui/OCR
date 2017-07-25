5e9007360a0ec4b77f76087ebc8eee316c3d4527
Doreenruirui <doreen.ruirui@gmail.com>
2017-07-25 10:14:13 -0400

    decode_line

--- decode_line.sh
+++
@@ -1,9 +0,0 @@
-source /home/dong.r/.bash_profile
-python decode_line.py \
-    --data_dir='/scratch/dong.r/Dataset/OCR/data/char_date_50_0/train/' \
-    --train_dir='/scratch/dong.r/Model/OCR/nlc/char_date_50_0/train/'  \
-    --start=$1 \
-    --end=$2 \
-    --beam_size=1000 \
-    --gpu_frac=1 \
-    --nthread=40 \
